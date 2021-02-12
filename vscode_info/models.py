347d6cd135b451d847e06c0c4787b4bc14d85541
Paul Lunn <paulilunn@gmail.com>
2021-02-12 12:18:13 +0000

    makefile change

--- vscode_info/models.py
+++ vscode_info/models.py
@@ -5,6 +5,5 @@ from django.utils import timezone
 # Create your models here.
 class VSCodeInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    last_name = models.CharField(max_length=30)
     opened = models.DateTimeField(default=timezone.now)
     closed = models.DateTimeField(default=timezone.now)
